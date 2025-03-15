c4847bc38a981eb9cfc1b3d98684e077451710c6
Shmaizer <valerashmaizer@gmail.com>
2025-03-15 09:43:37 +0300

    fix double R

--- main.py
+++ main.py
@@ -12,5 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Shmaizer from second branch')
+    # FIX FIX FIX
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
